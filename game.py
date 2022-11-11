a5c1e2a523dd0e2d67236efeb27dd15c0e606327
Will Segale <39059459+WilSegale@users.noreply.github.com>
2022-11-11 12:42:33 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,14 +3,23 @@ import os
 
 def start():
     print(f"You get out of bed and get dressed.",
-          f"You hear something ringing.")
-    print(f"Do you see whats ringing: {YesOrNO}?")
+          f"\nYou hear something ringing.")
+    
+    print(f"\nDo you see find what's ringing: {YesOrNO}?")
 
     choice = input(">>> ")
+    #You find whats ringin
     if choice in yes:
-        print('hello wrold')
-
-
+        print(f"You find what the ringing is, and find out that it's your phone.",
+              f"\n")
+              
+    #You leave your aparment
+    elif choice in no:
+        print(f"You walk out the door with out checking what the ring is")
+    
+    else:
+        os.system('clear')
+        return start()
 
 
 
