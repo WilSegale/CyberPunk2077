d7b738b31fe26d8a07d91ce4700a04d1365da14b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2022-11-12 15:21:52 -0500

    Update game.py

--- game.py
+++ game.py
@@ -34,7 +34,7 @@ def start():
       
 #player hanges up so they can go to work
 def HangUp():
-      print(f'''“Can I call you back? I have to go to work.” You say to your friend.''',
+      print(f'''\n“Can I call you back? I have to go to work.” You say to your friend.''',
             f'''\n“Ya sure.” He replies.''')
       outside();
 
@@ -46,12 +46,14 @@ def outside():
             f'''\n“You haven't left me yet have you.” You say to your old car.''')
       DriveToWork();
 
+#player goes to work and gets stuck in traffic
 def DriveToWork():
       print(f'''\nYou get on the highway and find that there is traffic.''',
             f'''\nYou look at your watch and see that the time is {TimeRightNow}.''',
             f'''\nAfter a while you get to work.''')
       work();
 
+#player gets into work and clocks in
 def work():
       print(f'''\nYou get to work and clock in like you do every day.''',
             f'''\nYou go to your desk and turn on your computer.''',
@@ -62,27 +64,39 @@ def work():
       print(f'''\nDo you get lunch with your co-workres. {YesOrNO}''')
       
       choice = input(">>> ")
+
       if choice in yes:
             getLunch();
+
       elif choice in no:
             getBackToWork()
+      
       else:
             print(f'''{InvalidInput}''')
+
+#player gets lunch with there co-workres
 def getLunch():
-      print(f'''"Ya sure I'll have lunch with you guys." You say to your co-workres.''',
+      print(f'''\n"Ya sure I'll have lunch with you guys." You say to your co-workres.''',
             f'''\nYou and your co-workres walk to get lunch at the nearest resteront.''',
             f'''\nYou sit down at your seat and talk to your co-workres.''',
             f'''\nAfter you finish eating you and your co-workres go back to work.''')
       BackToWork();
 
+#player doesnt go and get lunch with there co-workres
 def getBackToWork():
       print(f'''\n"Sorry I cant I have a lot of work to do. Mabye next time?"''')
       BackToWork()
 
+#player goes back to work and then gets called to the bosses office
 def BackToWork():
-      print(f'''You get back to work. 5 minutes later you hear your boss say''',
+      print(f'''\nYou get back to work. 5 minutes later you hear your boss say''',
             f'''\n"Hey {playerName} can I talk to you in my office?"''',
             f'''\nYou start to get scared that you did something wrong, but you keep your compuoser and say''',
             f'''\n"Ya sure Im coming right now."''')
+      GoToTheBossOffice();
+
+#player goes into the bosses office and gets talked too
+def GoToTheBossOffice():
+      print(f'''\n"I would like to talk to you about the sales that have happend." Your boss tells you.''')
   
 start()
